2639275492fa11ec7fb0279576d0fb3488a48967
ptfn <CipherDogs@protonmail.com>
2021-12-15 22:26:42 +0700

    added code

---
+++ wikibot.py
@@ -0,0 +1,47 @@
+import telebot, wikipedia, time, sys
+
+bot = telebot.TeleBot(sys.argv[1])
+
+def getwiki(text):
+    try:
+        wikipedia.set_lang("ru")
+        ny = wikipedia.page(text[6:])
+        return ny.url
+    except Exception as e:
+        try:
+            wikipedia.set_lang("eu")
+            ny = wikipedia.page(text[6:])
+            return ny.url
+        except Exception as e:
+            return "Article not found!"
+
+def getcont(text):
+    try:
+        wikipedia.set_lang("ru")
+        ny = wikipedia.page(text[9:])
+        string = ny.content
+        return string[:string.find("\n")]
+    except Exception as e:
+        return "Content not found!"
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Hello, i am wikibot.")
+
+@bot.message_handler(commands=["find"])
+def handle_page(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+
+@bot.message_handler(commands=["content"])
+def handle_cont(message):
+    bot.send_message(message.chat.id, getcont(message.text))
+
+def telegram_polling():
+    try:
+        bot.polling(none_stop=True, timeout=10)
+    except:
+        bot.stop_polling()
+        telegram_polling()
+
+if __name__ == "__main__":
+    telegram_polling()
